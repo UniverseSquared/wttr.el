e9cdab69dbc099aa990ff1bd5ef1e0a07bf216f5
UniverseSquared <universesquared4@gmail.com>
2019-10-05 23:44:11 +0100

    Add support for viewing moon phase, fix bug.
    Fixed bug where if the `wttr` command was used more than once without
    killing the `*wttr*` buffer, writing would not work correctly due to
    the buffer being read-only.

--- wttr.el
+++ wttr.el
@@ -3,20 +3,27 @@
 (defcustom wttr-city-names '("London" "Milton Keynes" "Paris")
   "A list of city names that can be chosen from with `wttr'.")
 
-(defun wttr-request (city-name)
-  (let ((request-url (concat "http://wttr.in/" city-name))
+(defun wttr-request (path)
+  (let ((request-url (concat "http://wttr.in/" path))
 	(url-request-extra-headers '(("User-Agent" . "curl"))))
     (with-current-buffer (url-retrieve-synchronously request-url)
       (goto-char (1+ url-http-end-of-headers))
       (decode-coding-string (buffer-substring (point) (point-max)) 'utf-8))))
 
-(defun wttr ()
-  (interactive)
-  (let ((city-name (completing-read "City name: " wttr-city-names))
-	(response (wttr-request city-name)))
+(defun wttr-show (path)
+  (let ((response (wttr-request path))
+	(inhibit-read-only t))
     (switch-to-buffer "*wttr*")
     (setq buffer-read-only t)
     (erase-buffer)
     (insert (xterm-color-filter response))))
+
+(defun wttr-moon ()
+  (interactive)
+  (wttr-show "moon"))
+
+(defun wttr ()
+  (interactive)
+  (wttr-show (completing-read "City name: " wttr-city-names)))
 
 (provide 'wttr)
